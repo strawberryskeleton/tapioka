0173a65134a276689c4536feafb0a66e639e4b5c
strawberryskeleton <xradhi5685@gmail.com>
2026-06-08 20:50:40 +0530

    landing/intro page completed + setup route paths

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tapioka/components/button.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 72, 46, 150),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(height: 25),
+
+            // shop name
+            Text(
+              'TAPIOKA',
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // icon
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('lib/images/shop_icon.png'),
+            ),
+
+            const SizedBox(height: 25),
+            // title
+            Text(
+              'THE TASTE OF REAL BOBA',
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 44,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            //subtitle
+            Text(
+              "Feel the taste of the most popular Bubble Tea from anywhere and anytime",
+              style: TextStyle(
+                color: Colors.grey[300],
+                height: 2,
+              ),
+            ),
+
+
+            const SizedBox(height: 25),
+
+            // button
+            MyButton(
+              text: "Get Started", 
+              onTap: () {
+                // go to menu page
+                Navigator.pushNamed(context, '/menupage');
+              },
+            )
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
